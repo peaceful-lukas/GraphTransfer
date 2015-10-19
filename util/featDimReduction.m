03ec88f509a53d0b5d6d4a9a20c310cdfcd81275
Taewoo Lukas Kim <peaceful.lukas@gmail.com>
2015-10-19 23:48:29 +0900

    bug fix

--- util/featDimReduction.m
+++ util/featDimReduction.m
@@ -3,13 +3,11 @@ function [DS param] = featDimReduction(DS, param, reducedDim)
 % train dataset
 X = DS.D;
 [~, pca_score, latent] = pca(X');
-[~, pca_score, ~] = pca(classProtos');
 reduced_X = pca_score(:, 1:reducedDim)';
 DS.D = reduced_X;
 
 X = DS.T;
 [~, pca_score, latent] = pca(X');
-[~, pca_score, ~] = pca(classProtos');
 reduced_X = pca_score(:, 1:reducedDim)';
 DS.T = reduced_X;
 
